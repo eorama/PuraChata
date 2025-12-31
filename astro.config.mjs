dbd1688031c5ba39d91c92bb150b8be1c6b674f0
Eliezer Orama <eliezerorama15@gmail.com>
2025-12-31 17:52:44 +0100

    Configure GitHub Pages deployment via Actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  site: 'https://eorama.github.io',
+  base: '/PuraChata',
   integrations: [react(), tailwind()],
   build: {
     format: 'file'
